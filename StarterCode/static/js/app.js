c3524c21f0340cd2bc7eca1cb03a19261a71d285
Jared Nieves <jared3194@gmail.com>
2021-04-01 12:04:31 -0400

    Populated table and created funtion to display filtered data based on user incput in the date filter input box.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,50 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+var fillTable = (dataInput) => {
+    
+    dataInput.forEach(sighting => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+    });
+});
+}
+
+fillTable(data);
+
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var filteredData = data.filter(sighting => sighting.datetime === inputValue);
+
+    console.log(filteredData);
+
+    tbody.html("");
+
+    let response = {
+        filteredData
+    }
+
+    if (response.filteredData.length !== 0) {
+        fillTable(filteredData);
+    }
+        else {
+            tbody.append("tr").append("td").text("No results found.");
+        }
+    
+      
+   
+};
+
